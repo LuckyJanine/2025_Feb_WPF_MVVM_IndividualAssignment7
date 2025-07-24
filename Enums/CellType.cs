d7b5d396a4d53348b2e54d99cc65b1d73a224baf
LJ_Tao <lingjie.tao63@gmail.com>
2025-07-24 21:05:43 +0200

    enums

---
+++ Enums/CellType.cs
@@ -0,0 +1,11 @@
+﻿namespace CellBank.Enums
+{
+    enum CellType
+    {
+        FIBRO, // Fibroblasts
+        HSPCs, // Hematopoietic Stem Progenitor Cells
+        Keratinocytes,
+        RTECs, // Renal tubular epithelial cells  
+        IPSCs // induced Pluripotent stem cells
+    }
+}
---
+++ Enums/SampleType.cs
@@ -0,0 +1,10 @@
+﻿namespace CellBank.Enums
+{
+    enum SampleType
+    {
+        Skin_Biopsy,
+        Peripheral_Blood,
+        Plucked_Hair,
+        urinary
+    }
+}
